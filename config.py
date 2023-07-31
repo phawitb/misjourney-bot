e4264dea335d2ded367da53fb637070e2e2f728d
phawitb <96680592+phawitb@users.noreply.github.com>
2023-07-31 16:05:06 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,92 @@
+username = 'phawit.boo@gmail.com'  #------------------------
+password = 'awevdfkbm'   #------------------------
+mode = '/relax'
+
+chrome_version = '114.0.5735.90'    #------------------------
+chrome_headless = True
+max_dimension = 400  # for resize image, Replace with the desired maximum dimension (width or height)
+
+waiting_time = 120
+
+nation = [
+    "Brunei",
+    "Cambodia",
+    "East Timor",
+    "Indonesia",
+    "Laos",
+    "Malaysia",
+    "Myanmar",
+    "Philippines",
+    "Singapore",
+    "Thailand",
+    "Vietnam"
+]
+location = [
+    "Bangkok",
+    "Chiang Mai",
+    "Phuket",
+    "Ayutthaya",
+    "Krabi",
+    "Pattaya",
+    "Koh Samui",
+    "Hua Hin",
+    "Sukhothai",
+    "Kanchanaburi",
+    "Phi Phi Islands",
+    "Ko Tao",
+    "Ko Pha Ngan",
+    "Chiang Rai",
+    "Ko Lanta",
+    "Railay Beach",
+    "Ko Chang",
+    "Ko Phi Phi Leh",
+    "Khao Sok National Park",
+    "Erawan National Park"
+]
+wearing_styles = [
+    "Casual",
+    "Formal",
+    "Business Casual",
+    "Smart Casual",
+    "Athleisure",
+    "Bohemian",
+    "Vintage",
+    "Gothic",
+    "Preppy",
+    "Streetwear",
+    "Hipster",
+    "Chic",
+    "Glamorous",
+    "Sporty",
+    "Retro",
+    "Punk",
+    "Classic",
+    "Eclectic",
+    "Rugged",
+    "Boho-Chic"
+]
+photo_perspectives_person = [
+    "Portrait",
+    "Candid",
+    "Profile",
+    "Close-up",
+    "Half-body",
+    "Full-body",
+    # "Headshot",
+    "Environmental Portrait",
+    "Bokeh (Background Blur)",
+    "Over-the-Shoulder",
+    # "Backlit",
+    # "Silhouette",
+    "Low Angle",
+    "High Angle",
+    # "Leading Lines",
+    # "Symmetrical",
+    # "Asymmetrical",
+    # "Framed",
+    # "Reflection",
+    # "Group Shot"
+]
+style = ['unofficial photo','from moblie camera','Take a normal photo','Take a friendly photo',' ']
+
+
